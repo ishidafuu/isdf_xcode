717486a2549285312b88d3c5c39dff8d1d41389e
ishidafuu <ishidafuu@gmail.com>
2020-10-04 19:15:19 +0900

    init

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Hiroaki Ishida on 2020/10/04.
+//  Copyright © 2020年 Hiroaki Ishida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Hiroaki Ishida on 2020/10/04.
+//  Copyright © 2020年 Hiroaki Ishida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
