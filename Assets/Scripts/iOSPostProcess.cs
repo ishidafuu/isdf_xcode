fb2642fca8c2d17859bc872154c043b873d57bd4
ishidafuu <ishidafuu@gmail.com>
2020-10-07 23:07:23 +0900

    iOSPostProcess追加

---
+++ Assets/Scripts/iOSPostProcess.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Callbacks;
+#if UNITY_IOS
+using UnityEditor.iOS.Xcode;
+namespace NK
+{
+    public static class iOSPostProcess
+    {
+        [PostProcessBuild]
+        private static void OnPostProcessBuild( BuildTarget buildTarget, string path )
+        {
+            if ( buildTarget != BuildTarget.iOS ) return;
+
+            var projectPath = PBXProject.GetPBXProjectPath( path );
+            var project = new PBXProject();
+            project.ReadFromFile( projectPath );
+            
+            string[] targetGuids = { project.GetUnityMainTargetGuid(), 
+                project.GetUnityFrameworkTargetGuid() };
+
+            project.SetBuildProperty(targetGuids, "ENABLE_BITCODE", "NO");
+
+            project.WriteToFile( projectPath );
+        }
+    }
+}
+#endif
+#endif
